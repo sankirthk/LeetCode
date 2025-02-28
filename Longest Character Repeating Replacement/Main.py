dad2effb2aa7ea150fac77ca3caa6f85dd806e9d
sankirthk <sankirthk@gmail.com>
2025-02-28 10:53:43 -0500

    Sliding Window Problems

---
+++ Longest Character Repeating Replacement/Main.py
@@ -0,0 +1,24 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        res = 0
+        
+        l = 0
+        maxf = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]])
+
+            while (r - l + 1) - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+
+        return res
+    
+if __name__ == "__main__":
+    input = "AAABABB"
+    k = 1
+    solution = Solution()
+    print("Input: ", input)
+    print("Output: ", solution.characterReplacement(input, k))
---
+++ Longest Substring Without Repeating Characters/Main.py
@@ -0,0 +1,20 @@
+from typing import List, Optional
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charCount = {}
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            if s[r] in charCount:
+                l = max(charCount[s[r]] + 1, l)
+            charCount[s[r]] = r
+            res = max(res, r - l + 1)
+        return res
+            
+if __name__ == "__main__":
+    s = "abcabcbb"
+    solution = Solution()
+    print("Input: ", s)
+    print("Output: ", solution.lengthOfLongestSubstring(s))
