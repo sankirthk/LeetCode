200e73b4e73a568f8ce2983035deaad46f6915ca
sankirthk <sankirthk@gmail.com>
2025-04-14 23:49:56 -0400

    Invert Binary Tree

--- Invert Binary Tree/Main.py
+++ Invert Binary Tree/Main.py
@@ -1,28 +1,56 @@
 from typing import List, Optional 
+from collections import deque
 
 class TreeNode:
-  def __init__(self, val=0, left=None, right=None):
+    def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+    @staticmethod
+    def from_list(values: List[Optional[int]]) -> Optional['TreeNode']:
+        if not values:
+            return None
+
+        root = TreeNode(values[0])
+        queue = deque([root])
+        i = 1
+
+        while queue and i < len(values):
+            current = queue.popleft()
+
+            if values[i] is not None:
+                current.left = TreeNode(values[i])
+                queue.append(current.left)
+            i += 1
+
+            if i < len(values) and values[i] is not None:
+                current.right = TreeNode(values[i])
+                queue.append(current.right)
+            i += 1
+
+        return root
+
 
 
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
-            return root
+            return None
         
-        leftTemp, rightTemp = None, None
-        curr = root
-        leftTemp = curr.left
-        rightTemp = curr.right
-        curr.left = rightTemp
-        curr.right = leftTemp
-        if curr.left:
-            self.invertTree(curr.left)
-        if curr.right:
-            self.invertTree(curr.right)
-        
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
         return root
+
+if __name__ == "__main__":
+    values = [1,2,3,4,5,6,7]
+    bt = TreeNode.from_list(values)
+    solution = Solution()
+    print(f"Output is: {solution.invertTree(bt)}")
+
+
             
         
